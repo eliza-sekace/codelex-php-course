00a278e9ec05a0a69882f2cd1af6e2da76319aa6
Eliza Sekace <elizasekace@gmail.com>
2022-01-10 20:33:01 +0200

    Added tic-tac-toe first version

--- arrays/05_arrays_tictactoe.php
+++ arrays/05_arrays_tictactoe.php
@@ -6,58 +6,79 @@ $gameBoard = [
     ["-", "-", "-"]
 ];
 
-function display_board($gameBoard)
-{
-    echo "   |   |   \n";
-    echo "---+---+---\n";
-    echo "   |   |   \n";
-    echo "---+---+---\n";
-    echo "   |   |   \n";
-}
-
-display_board($gameBoard);
 
 $turn = "X";
-$moveCount = 0;
 
-//$winnerCombos = [
-//    [[0][0], [0][1], [0][2]],
-//    [[1][0], [1][1], [1][2]],
-//    [[2][0], [2][1], [2][2]],
-//    [[0][0], [1][1], [2][2]],
-//    [[2][0], [1][1], [0][2]],
-//    [[0][0], [1][0], [2][0]],
-//    [[0][1], [1][1], [2][1]],
-//    [[0][2], [1][2], [2][2]],
-//];
+$winnerCombos = [
+    //rindas
+    [[0, 0], [0, 1], [0, 2]],
+    [[1, 0], [1, 1], [1, 2]],
+    [[2, 0], [2, 1], [2, 2]],
+    //diagonales
+    [[0, 0], [1, 1], [2, 2]],
+    [[2, 0], [1, 1], [0, 2]],
+    //kolonnas
+    [[0, 0], [1, 0], [2, 0]],
+    [[0, 1], [1, 1], [2, 1]],
+    [[0, 2], [1, 2], [2, 2]],
+];
 
-
-while ($moveCount < 9) {
-    for ($x = 0; $x < 3; $x++) {
-        $row = readline("It's " . $turn . " turn. Choose a row: \n");
-        $column = readline(" Choose a column: \n");
+echo getFormattedBoard($gameBoard);
+while (!isFull($gameBoard) && getWinner($gameBoard, $winnerCombos) === "-") {
+    $row = readline("It's " . $turn . " turn. Choose a row: \n");
+    $column = readline(" Choose a column: \n");
+    if ($gameBoard[$row][$column] === "-") {
         $gameBoard[$row][$column] = $turn;
-        for ($i = 0; $i < 3; $i++) {
-            for ($j = 0; $j < 3; $j++) {
-                echo " " . $gameBoard[$i][$j] . " ";
-            }
-            echo "\n";
-        }
         if ($turn === "X") {
             $turn = "O";
         } else {
             $turn = "X";
         }
-        $moveCount++;
     }
-    echo "Its " . $turn . " turn";
+    echo getFormattedBoard($gameBoard);
+}
+
+if (getWinner($gameBoard, $winnerCombos) !== "-") {
+    echo "Winner is " . getWinner($gameBoard, $winnerCombos) . "!\n";
+} else {
+    echo "It's a tie!";
 }
 
 
-//win
-//tie
-//lose
-//ka nevar uzlikt uz jau aiznemta laucina
+function getWinner($gameBoard, $winnerCombos)
+{
+    foreach ($winnerCombos as $combination) {
+        if ($gameBoard[$combination[0][0]][$combination[0][1]] === $gameBoard[$combination[1][0]][$combination[1][1]] &&
+            $gameBoard[$combination[1][0]][$combination[1][1]] === $gameBoard[$combination[2][0]][$combination[2][1]] &&
+            $gameBoard[$combination[1][0]][$combination[1][1]] !== "-") {
+            return $gameBoard[$combination[0][0]][$combination[0][1]];
+        }
+    }
+    return "-";
+}
 
+function isFull($gameBoard)
+{
+    $isFull = true;
+    foreach ($gameBoard as $row) {
+        foreach ($row as $cell) {
+            if ($cell === "-") {
+                $isFull = false;
+            }
+        }
+    }
+    return $isFull;
+}
 
+function getFormattedBoard($gameBoard)
+{
+    $output = '';
+    for ($i = 0; $i < 3; $i++) {
+        for ($j = 0; $j < 3; $j++) {
+            $output .= " " . $gameBoard[$i][$j] . " ";
+        }
+        $output .= "\n";
+    }
+    return $output;
+}
 
